2229ff1c1f70ba49883e1eb5ed110dc50eaa9188
Abhishek Tomar <6933841+ImAbhishekTomar@users.noreply.github.com>
2021-11-22 00:33:23 +0530

    Changes

--- App.tsx
+++ App.tsx
@@ -14,9 +14,11 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
+import RazorpayCheckout from 'react-native-razorpay';
 import {
   Colors,
   DebugInstructions,
@@ -37,28 +39,30 @@ import {
 console.log(process.env.RAZORPAY_KEY_ID);
 console.log(process.env.RAZORPAY_KEY_SECRET);
 
-const Section = ({children, title}): Node => {
+const Section = (props: {children: any; title: any; onPress?(): any}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <TouchableOpacity onPress={props.onPress}>
+        <Text
+          style={[
+            styles.sectionTitle,
+            {
+              color: isDarkMode ? Colors.white : Colors.black,
+            },
+          ]}>
+          {props.title}
+        </Text>
+        <Text
+          style={[
+            styles.sectionDescription,
+            {
+              color: isDarkMode ? Colors.light : Colors.dark,
+            },
+          ]}>
+          {props.children}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -70,6 +74,32 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const onPay = () => {
+    const options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: 'INR',
+      key: 'enter your key',
+      amount: '5000',
+      name: 'Acme Corp',
+      order_id: 'order_DslnoIgkIDL8Zt', //Replace this with an order_id created using Orders API.
+      prefill: {
+        email: 'abhi.tomar@live.com',
+        contact: '0917840000702',
+        name: 'Mr. Sample Payment',
+      },
+      theme: {color: '#53a20e'},
+    };
+    RazorpayCheckout.open(options)
+      .then((data: any) => {
+        console.info('Success!!', JSON.stringify(data));
+        alert(`Success: ${data.razorpay_payment_id}`);
+      })
+      .catch(function (error: {code: any; description: any}) {
+        alert(`Error: ${error.code} | ${error.description}`);
+      });
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -78,20 +108,20 @@ const App: () => Node = () => {
         style={backgroundStyle}>
         <Header />
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
+          style={{backgroundColor: isDarkMode ? Colors.black : Colors.white}}>
+          <Section title="Pay Now" onPress={onPay}>
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
+          <Section
+            title="See Your Changes"
+            onPress={() => console.log('click')}>
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
+          <Section title="Learn More" onPress={() => console.log('click')}>
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
